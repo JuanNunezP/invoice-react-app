cf76ac6ca6fc5f8098faab5446fadc384b8b6728
Juan N <jnuezpinilla@gmail.com>
2025-10-05 10:13:19 -0500

    Quito funcion no usada que estaba comentada

--- src/services/getInvoice.js
+++ src/services/getInvoice.js
@@ -2,11 +2,6 @@ import { invoice } from "../data/Invoice"
 
 export const getInvoice = () => {
 
-    // let total=0;
-    // invoice.items.forEach(item => { 
-    //     total = total + item.price * item.quantity
-
-    // })
 const total = invoice.items.reduce((accumulator, item) => {
     const itemTotal = item.price * item.quantity;
     return accumulator + itemTotal;
